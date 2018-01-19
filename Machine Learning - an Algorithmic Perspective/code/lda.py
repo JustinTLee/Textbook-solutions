9b253075c58d4a50b1e586d8b68ee9c2043f95a3
Justin Lee <justinlee.gwu@gmail.com>
2018-01-19 15:52:12 -0500

    add eigenvalue sorting to LDA code

--- Machine Learning - an Algorithmic Perspective/code/lda.py
+++ Machine Learning - an Algorithmic Perspective/code/lda.py
@@ -25,17 +25,22 @@ for x in vectClass:
     pC = np.sum(vectLabels == x)/np.size(vectLabels)
     
     # logically select all rows with class x
-    vectClassLogicals = np.squeeze(vectLabels == x)
+    boolClass = np.squeeze(vectLabels == x)
         
     # create the variance within-class matrix (SW)
-    matSWC = np.cov(np.transpose(matData[vectClassLogicals, :]))
-    matSW = matSW + pC*np.cov(np.transpose(matData[vectClassLogicals, :]))
+    matSWC = np.cov(np.transpose(matData[boolClass, :]))
+    matSW = matSW + pC*np.cov(np.transpose(matData[boolClass, :]))
     
     # mean-deviation from data mean
-    vectMeanDev = np.reshape(np.mean(matData[vectClassLogicals, :], axis = 0), (nFeatVar, 1)) - vectMean
+    vectMeanDev = np.reshape(np.mean(matData[boolClass, :], axis = 0), (nFeatVar, 1)) - vectMean
 
     # create variance between-class matrix (SB)
     matSB = matSB + np.dot(vectMeanDev, np.transpose(vectMeanDev))
 
 # solve generalized eigenvalue problem for the ratio of between-class to within-class (SB/SW)
 vectEigVal, matEigVect = la.eig(la.inv(matSW).dot(matSB))
+
+boolEigInd = np.argsort(vectEigVal)
+boolEigInd = boolEigInd[::-1]
+matEigVect = matEigVect[:, boolEigInd]
+vectEigVal = vectEigVal[boolEigInd]
