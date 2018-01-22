85035bc40cb731930ac8ca17d900b9d279d088d1
Justin Lee <justinlee.gwu@gmail.com>
2018-01-22 14:34:42 -0500

    Add tests for MLP class

--- Machine Learning - an Algorithmic Perspective/code/pcn_test.py
+++ Machine Learning - an Algorithmic Perspective/code/pcn_test.py
@@ -9,6 +9,7 @@ import unittest
 #                                        - predicting properly
 # 2018-01-21 - JL - split up class test cases between new neuron clsas and pcn class
 #                 - create new test cases for neuron class
+# 2018-01-22 - JL - added tests for new MLP class
 
 class TestNeuronClass(unittest.TestCase):
 
@@ -22,7 +23,10 @@ class TestNeuronClass(unittest.TestCase):
         self.assertEqual(self.test.nInputs, 3)
 
     def test_seed_init_1(self):
-        self.assertEqual(self.test.seed, 20180121)
+        dtNow = dt.datetime.now()
+        numNow = int(str(dtNow.year) + str(dtNow.month).zfill(2) + str(dtNow.day).zfill(2))
+        test2 = pcn.neuron(1)
+        self.assertEqual(self.test.seed, numNow)
 
     def test_seed_init_2(self):
         dtNow = dt.datetime.now()
@@ -31,7 +35,7 @@ class TestNeuronClass(unittest.TestCase):
         self.assertEqual(test2.seed, numNow)
 
     def test_thresh_type_init_1(self):
-        self.assertEqual(self.test.thresh_type, 'linear')
+        self.assertEqual(self.test.thresh_type, 'step')
 
     def test_thresh_type_init_2(self):
         test2 = pcn.neuron(2, thresh_type = "logistic")
@@ -47,8 +51,8 @@ class TestNeuronClass(unittest.TestCase):
 
         # test logistic thresholding
         test2 = pcn.neuron(2, thresh_type = "logistic")
-        self.assertEqual(test2.thresholdH(0.9), 1)
-        self.assertEqual(test2.thresholdH(-0.3), 0)
+        self.assertEqual(test2.thresholdH(0.9) > 0.5, 1)
+        self.assertEqual(test2.thresholdH(-0.3) > 0.5, 0)
 
     def test_predictLabels(self):
         self.assertEqual(np.shape(self.test.predictLabels(self.data)), (4, 1))
@@ -87,7 +91,12 @@ class TestPCNClass(unittest.TestCase):
         self.assertEqual(self.test_pcn1.thresh_type, 'logistic')
 
     def test_thresh_type_init_2(self):
-        self.assertEqual(self.test_pcn2.thresh_type, 'linear')
+        self.assertEqual(self.test_pcn2.thresh_type, 'step')
+
+    def test_initializeneurons(self):
+        test_pcn3 = pcn.pcn(1)
+        test_pcn3.initializeNeurons(np.shape(self.data)[1])
+        self.assertEqual(np.shape(test_pcn3.matNeurons[0].weights), (3, 1))
 
     def test_weights_dim(self):
         self.assertEqual(np.shape(self.test_pcn1.matNeurons[0].weights), (3, 1))
@@ -104,7 +113,28 @@ class TestPCNClass(unittest.TestCase):
         targets3 = np.array([0, 1, 1, 0])
         test_pcn3 = pcn.pcn(1, iter = 30)
         predict3 = np.squeeze(test_pcn3.trainWeights(data3D, targets3))
-        np.testing.assert_equal(predict3.tolist(), targets3)    
+        np.testing.assert_equal(predict3.tolist(), targets3)
+
+class TestMLPClass(unittest.TestCase):
+
+    def setUp(self):
+        self.test_mlp = pcn.mlp([2, 2, 1], seed = 20180122, iter = 200)
+        self.data = np.matrix([[0, 0], [0, 1], [1, 0], [1, 1]])
+        self.nFeatvar = np.shape(self.data)[1]
+
+    def test_layers_init(self):
+        self.assertEqual(self.test_mlp.nLayers, 3)
+
+    def test_nNeurons_init(self):
+        self.assertEqual(self.test_mlp.matPCN[0].nNeurons, 2)
+        self.assertEqual(self.test_mlp.matPCN[1].nNeurons, 2)
+        self.assertEqual(self.test_mlp.matPCN[2].nNeurons, 1)
+
+    def test_predict1(self):
+        test_mlp2 = pcn.mlp([20, 1], iter = 8000)
+        targets = np.array([[0], [1], [1], [0]])
+        output = test_mlp2.trainWeights(self.data, targets)
+        np.testing.assert_equal(output.tolist(), targets)
 
 
 if __name__ == '__main__':
