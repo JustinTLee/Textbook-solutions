7dca1ba0f29fed556a5c76d5809d43ea28510483
Justin Lee <justinlee.gwu@gmail.com>
2018-01-25 15:10:04 -0500

    Add test cases for RBFNeuron class

--- Machine Learning - an Algorithmic Perspective/code/rbf_test.py
+++ Machine Learning - an Algorithmic Perspective/code/rbf_test.py
@@ -2,10 +2,31 @@ import numpy as np
 import rbf
 import unittest
 
+class TestRBFNeuronClass(unittest.TestCase):
+
+	def setUp(self):
+		self.neuron1 = rbf.rbfneuron(1, 30)
+
+	def test_nInputs_init(self):
+		self.assertEqual(self.neuron1.nInputs, 1)
+
+	def test_std_init(self):
+		self.assertEqual(self.neuron1.sigma, 30)
+
+	def test_setNone_init(self):
+		self.assertEqual(self.neuron1.seed, None)
+		self.assertEqual(self.neuron1.momentum, None)
+		self.assertEqual(self.neuron1.thresh_type, None)
+
+	def test_predictLabels(self):
+		datapoint1 = np.matrix([[0]])
+		self.neuron1.weights = np.matrix([[0]])
+		self.assertEqual(self.neuron1.predictLabels(datapoint1), 1)
+
 class TestRBFClass(unittest.TestCase):
 
     def setUp(self):
-        self.rbf_test1 = rbf.rbf(eta = 0.25, nIter = 100)
+        self.rbf_test1 = rbf.rbf(1, 20, eta = 0.25, nIter = 100)
 
     def test_eta_init(self):
         self.assertEqual(self.rbf_test1.eta, 0.25)
